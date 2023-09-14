a913236755ac1261dc903086c8ce23a9158e8128
Inioluwa Adewara <adewarainioluwa@gmail.com>
2023-09-14 11:31:04 +0100

    update config

--- server/app/config.py
+++ server/app/config.py
@@ -9,14 +9,14 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    JWT_COOKIE_SECURE = False  #set to true in production
+    JWT_COOKIE_SECURE = True  #set to true in production
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
-    JWT_COOKIE_CSRF_PROTECT = False
+    JWT_COOKIE_CSRF_PROTECT = True
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_ACCESS_COOKIE_NAME = "access_token_cookie"
     JWT_COOKIE_DOMAIN = "127.0.0.1"
-    JWT_COOKIE_SAMESITE = "None"
+    JWT_COOKIE_SAMESITE = "Lax"
     JWT_ACCESS_COOKIE_PATH = "/"
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
